8689ff860294bc422087115fef26eac769e6502c
JJtopink <1102108104@gm.kuas.edu.tw>
2018-04-01 21:56:03 +0800

    v1.1
    *按鈕數量擴充至16個
    *新增一次補齊按鈕功能

--- 動態新增元件/動態新增元件/MainWindow.xaml.cs
+++ 動態新增元件/動態新增元件/MainWindow.xaml.cs
@@ -20,13 +20,13 @@ namespace 動態新增元件
     /// </summary>
     
         /*下一新增內容
-        *按鈕數量擴充至16個(陣列用RESIZE方法)
+        *按鈕數量擴充至16個
         *新增一次補齊按鈕功能
          */
     public partial class MainWindow : Window
     {
-        Button[] b_arr = new Button[5];
-        int bt_count=0,temp_i=0;
+        Button[] b_arr = new Button[16];
+        int bt_count=0;
 
 
         public MainWindow()
@@ -41,74 +41,72 @@ namespace 動態新增元件
                 b_arr[i] = empty;
             }
         }
+
+        public Button setbutton(Button bt,int height,int width,String name,String content) {
+            bt.Height = height;
+            bt.Width = width;
+            bt.Name = name;
+            bt.Content = content;
+            return bt;
+        }
         
         private void bt_add_Click(object sender, RoutedEventArgs e)
         {
             Button b1 = new Button();
             
-            b1.Height = 100;
-            b1.Width = 100;
             b1.VerticalAlignment = VerticalAlignment.Top;
             b1.HorizontalAlignment = HorizontalAlignment.Left;
 
             //新增按鈕，存進陣列
             for (int i = 0; i < b_arr.Length; i++) {
                 if (b_arr[i].Name=="empty"+Convert.ToString(i)) {
-                    b1.Content = "按鈕" + (i+1);
-                    b1.Name = "bt" + Convert.ToString(i);
+                    b1 = setbutton(b1,100,100, "bt" + Convert.ToString(i), "按鈕" + (i + 1));
                     b1.Click += new RoutedEventHandler(remove);//按鈕事件=>移除按鈕
                     b_arr[i] = b1;
-                    temp_i = i;//暫存i
+                    b1.Margin = new Thickness(35 + 115 * (i % 4), 130 + 120 * (i / 4), 0, 0);
                     break;
                 }
             }
-
-            //利用暫存i來選取按鈕位置
-            switch (temp_i) {
-                case 0:
-                    b1.Margin = new Thickness(35, 130, 0, 0);
-                    break;
-
-                case 1:
-                    b1.Margin = new Thickness(150, 130, 0, 0);
-                    break;
-
-                case 2:
-                    b1.Margin = new Thickness(265, 130, 0, 0);
-                    break;
-
-                case 3:
-                    b1.Margin = new Thickness(380, 130, 0, 0);
-                    break;
-
-                case 4:
-                    b1.Margin = new Thickness(35, 250, 0, 0);
-                    break;
-            }
+            
             gr1.Children.Add(b1);//將按鈕加進GRID
-
             bt_count += 1;//按鈕數量
 
-            /*if (bt_count == 5)
+            if (bt_count == 16)
             {
                 bt_add.IsEnabled = false;
             }
             else
             {
                 bt_add.IsEnabled = true;
-            }*/
+            }
+        }
+
+        //按鈕全補
+        private void bt_all_Click(object sender, RoutedEventArgs e)
+        {
+            
+            for (int i = 0; i < b_arr.Length; i++) {
+                if (b_arr[i].Name == "empty"+Convert.ToString(i)) {
+                    b_arr[i].VerticalAlignment = VerticalAlignment.Top;
+                    b_arr[i].HorizontalAlignment = HorizontalAlignment.Left;
+                    b_arr[i] = setbutton(b_arr[i], 100, 100, "bt" + Convert.ToString(i), "按鈕" + (i + 1));
+                    b_arr[i].Click += new RoutedEventHandler(remove);
+                    b_arr[i].Margin = new Thickness(35 + 115 * (i % 4), 130 + 120 * (i / 4), 0, 0); ;
+                    gr1.Children.Add(b_arr[i]);
+                }
+            }
         }
 
         private void remove(object sender, RoutedEventArgs e) {
             bt_count -= 1;
-            /*if (bt_count == 5)
+            if (bt_count == 16)
             {
                 bt_add.IsEnabled = false;
             }
             else
             {
                 bt_add.IsEnabled = true;
-            }*/
+            }
 
             Button b2 = (Button)sender;
 
